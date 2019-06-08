7e3c982e9064db521752c6f85cbfa02ae21c1227
Christoph Matscheko <christoph.matscheko@gmx.net>
2019-06-08 21:02:15 +0200

    Add buttons to main menu

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
 export class HomePage {
+  private static readonly VERSION = "3.0.0a1";
 
+  get appTitle(): string {
+    return `BowBuddy ${HomePage.VERSION}`;
+  }
 }
