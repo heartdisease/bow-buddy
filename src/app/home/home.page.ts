2acd7e25bf02f4f3776009a8fbec6348dd1c9492
Christoph Matscheko <christoph.matscheko@gmx.net>
2019-06-20 11:13:52 +0200

    Configure Prettier and format all code files accordingly

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,12 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "app-home",
-  templateUrl: "home.page.html",
-  styleUrls: ["home.page.scss"]
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  private static readonly VERSION = "3.0.0a1";
+  private static readonly VERSION = '3.0.0a1';
 
   get appTitle(): string {
     return `BowBuddy ${HomePage.VERSION}`;
